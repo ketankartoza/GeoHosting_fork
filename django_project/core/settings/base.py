8252d7d7547aa9953a5f53fd23ded5c77646e51c
Dimas Ciputra <dimas@kartoza.com>
2024-08-12 14:43:54 +0100

    Update STATICFILES_FINDERS (#72)

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -113,7 +113,6 @@ TEMPLATES = [
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 INSTALLED_APPS = (
