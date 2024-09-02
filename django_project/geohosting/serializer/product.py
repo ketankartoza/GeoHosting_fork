4034bd22b0581a9b609e32c2086ace7316ad83fa
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-09-02 09:15:43 +0200

    patch: return product meta (#107)
    * patch: return product meta
    
    * Update product.py

--- django_project/geohosting/serializer/product.py
+++ django_project/geohosting/serializer/product.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
-
-from geohosting.models import (
-    Product, ProductMedia, Package
-)
+from geohosting.models import Product, ProductMedia, Package, ProductMetadata
 
 
 class ProductMediaSerializer(serializers.ModelSerializer):
@@ -17,17 +14,25 @@ class ProductPackageSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ProductMetadataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductMetadata
+        fields = ['key', 'value']
+
+
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
             'id', 'name', 'order', 'upstream_id',
-            'description', 'image', 'available']
+            'description', 'image', 'available'
+        ]
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     images = ProductMediaSerializer(many=True, read_only=True)
     packages = serializers.SerializerMethodField()
+    product_meta = serializers.SerializerMethodField()
 
     def get_packages(self, obj: Product):
         packages = obj.packages.all()
@@ -41,8 +46,8 @@ class ProductDetailSerializer(serializers.ModelSerializer):
                 current_package = unique_packages[package.name]
                 for currency in preferred_currency_order:
                     if (
-                            package.currency == currency and
-                            current_package.currency != currency
+                        package.currency == currency and
+                        current_package.currency != currency
                     ):
                         unique_packages[package.name] = package
                         break
@@ -52,10 +57,19 @@ class ProductDetailSerializer(serializers.ModelSerializer):
             key=lambda p: (
                 'large' in p.name.lower(),
                 'medium' in p.name.lower(),
-                'small' in p.name.lower()))
+                'small' in p.name.lower()
+            )
+        )
 
         return ProductPackageSerializer(sorted_packages, many=True).data
+
+    def get_product_meta(self, obj: Product):
+        metadata = ProductMetadata.objects.filter(product=obj)
+        return ProductMetadataSerializer(metadata, many=True).data
 
     class Meta:
         model = Product
         fields = '__all__'
+        extra_kwargs = {
+            'product_meta': {'source': 'get_product_meta'}
+        }
